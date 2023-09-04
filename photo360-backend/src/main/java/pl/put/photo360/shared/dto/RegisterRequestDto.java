dc9d74b3f6bced06548969732e71c83d6e0af59d
Jakub Czajka <jczajka@psi.pl>
2023-09-04 14:11:42 +0200

    Added auth tests

--- photo360-backend/src/main/java/pl/put/photo360/shared/dto/RegisterRequestDto.java
+++ photo360-backend/src/main/java/pl/put/photo360/shared/dto/RegisterRequestDto.java
@@ -1,5 +1,6 @@
 package pl.put.photo360.shared.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequestDto
 {
     private String login;
--- photo360-backend/src/main/java/pl/put/photo360/shared/dto/RequestResponseDto.java
+++ photo360-backend/src/main/java/pl/put/photo360/shared/dto/RequestResponseDto.java
@@ -1,6 +1,7 @@
 package pl.put.photo360.shared.dto;
 
-import lombok.EqualsAndHashCode;
+import java.util.Objects;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,9 +10,8 @@ import lombok.Setter;
  * Simple class which contains a message as response to frontend.
  */
 @Getter
-@EqualsAndHashCode
-@NoArgsConstructor
 @Setter
+@NoArgsConstructor
 public class RequestResponseDto
 {
     private String responseMessage;
@@ -25,5 +25,23 @@ public class RequestResponseDto
         statusType = aServerResponse.getStatus()
             .getReasonPhrase();
         responseMessage = aServerResponse.getResponseMessage();
+    }
+
+    @Override
+    public boolean equals( Object aO )
+    {
+        if( this == aO )
+            return true;
+        if( !(aO instanceof RequestResponseDto that) )
+            return false;
+        return getStatusCode() == that.getStatusCode()
+            && Objects.equals( getResponseMessage(), that.getResponseMessage() )
+            && Objects.equals( getStatusType(), that.getStatusType() );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( getResponseMessage(), getStatusType(), getStatusCode() );
     }
 }

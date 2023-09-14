74de6a26681c8ee54ee06a7ab58a69f2c9136214
Jakub Czajka <jczajka@psi.pl>
2023-09-14 16:11:10 +0200

    Added documentation and remove functionality

--- photo360-backend/src/main/java/pl/put/photo360/Photo360Application.java
+++ photo360-backend/src/main/java/pl/put/photo360/Photo360Application.java
@@ -1,13 +1,25 @@
 package pl.put.photo360;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
-@SpringBootApplication
+@SpringBootApplication( scanBasePackages = "pl.put.photo360" )
 public class Photo360Application
 {
+    private static final Logger logger = LoggerFactory.getLogger( Photo360Application.class );
+
     public static void main( String[] args )
     {
+        logger.info( "----------------------------- PROPERTIES -----------------------------" );
+        logger.info( "Rest api documentation yaml version : http://localhost:8095/v3/api-docs.yaml" );
+        logger.info( "Rest api documentation yaml version reader: https://editor.swagger.io/" );
+        logger.info( "Rest api documentation : http://localhost:8095/swagger-ui/index.html#/" );
+        logger.info( "Springboot version  : " + SpringBootVersion.getVersion() );
+        logger.info( "Spring version  : " + SpringVersion.getVersion() );
         SpringApplication.run( Photo360Application.class, args );
     }
 }
--- photo360-backend/src/main/java/pl/put/photo360/controller/SystemController.java
+++ photo360-backend/src/main/java/pl/put/photo360/controller/SystemController.java
@@ -1,5 +1,6 @@
 package pl.put.photo360.controller;
 
+import static pl.put.photo360.shared.dto.ServerResponseCode.STATUS_GIF_REMOVED;
 import static pl.put.photo360.shared.dto.ServerResponseCode.STATUS_PHOTO_UPLOADED;
 
 import java.util.Collection;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import pl.put.photo360.service.PhotoService;
 import pl.put.photo360.shared.dto.PhotoDataDto;
 import pl.put.photo360.shared.dto.RequestResponseDto;
@@ -26,6 +29,7 @@ import pl.put.photo360.tokenValidator.annotation.RequiredRole;
 
 @RequestMapping( "/photo360" )
 @RestController( "SystemController" )
+@Tag( name = "Photo360 main functions controller" )
 public class SystemController
 {
     private final PhotoService photoService;
@@ -38,7 +42,7 @@ public class SystemController
 
     @PostMapping( "/uploadPhotos" )
     @RequiredRole( role = UserRoles.USER_ROLE )
-    @ApiOperation( "Endpoint to upload user's photos." )
+    @Operation( summary = "Endpoint to upload user's photos." )
     public ResponseEntity< RequestResponseDto > uploadPhoto(
         @RequestParam( value = "zipFile" ) MultipartFile aFile,
         @RequestParam( value = "isPublic" ) Boolean isPublic,
@@ -52,7 +56,7 @@ public class SystemController
 
     @GetMapping( "/downloadGif/{gifId}" )
     @RequiredRole( role = UserRoles.USER_ROLE )
-    @ApiOperation( "Endpoint to get specific public gif or private owned by logged user by id." )
+    @Operation( summary = "Endpoint to get specific public gif or private owned by logged user by id." )
     public ResponseEntity< PhotoDataDto > downloadGif(
         @RequestHeader( name = HttpHeaders.AUTHORIZATION ) String authorizationToken,
         @PathVariable Long gifId )
@@ -63,7 +67,7 @@ public class SystemController
 
     @GetMapping( "/downloadPublicGifs" )
     @RequiredRole( role = UserRoles.USER_ROLE )
-    @ApiOperation( "Endpoint to get all public gifs." )
+    @Operation( summary = "Endpoint to get all public gifs." )
     public ResponseEntity< Collection< PhotoDataDto > > downloadPublicGif()
     {
         var publicGifs = photoService.downloadPublicGifs();
@@ -72,7 +76,7 @@ public class SystemController
 
     @GetMapping( "/downloadPrivateGifs" )
     @RequiredRole( role = UserRoles.USER_ROLE )
-    @ApiOperation( "Endpoint to get all private gifs, which are owned by logged user." )
+    @Operation( summary = "Endpoint to get all private gifs, which are owned by logged user." )
     public ResponseEntity< Collection< PhotoDataDto > > downloadPrivateGif(
         @RequestHeader( name = HttpHeaders.AUTHORIZATION ) String authorizationToken )
     {
@@ -82,10 +86,22 @@ public class SystemController
 
     @GetMapping( "/downloadAllGifs" )
     @RequiredRole( role = UserRoles.ADMIN_ROLE )
-    @ApiOperation( "Endpoint to get all gifs in database, allowed only for admin user." )
+    @Operation( summary = "Endpoint to get all gifs in database, allowed only for admin user." )
     public ResponseEntity< Collection< PhotoDataDto > > downloadAllGif()
     {
         var gifs = photoService.downloadAllGifs();
         return new ResponseEntity<>( gifs, HttpStatus.OK );
+    }
+
+    @DeleteMapping( "/removeUserGif/{gifId}" )
+    @RequiredRole( role = UserRoles.USER_ROLE )
+    @Operation( summary = "Endpoint to remove gif, which is owned by logged user, also allows to remove any gif, if user has admin role" )
+    public ResponseEntity< RequestResponseDto > removeUserGif(
+        @RequestHeader( name = HttpHeaders.AUTHORIZATION ) String authorizationToken,
+        @PathVariable Long gifId )
+    {
+        photoService.removeUserGif( authorizationToken, gifId );
+        return new ResponseEntity<>( new RequestResponseDto( STATUS_GIF_REMOVED ),
+            STATUS_GIF_REMOVED.getStatus() );
     }
 }

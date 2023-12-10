d1df78868f6e04ef490316fcdcb2d5af183b27f6
JCz <jakub.czajka.00@gmail.com>
2023-12-10 19:57:10 +0100

    fixed bug

--- photo360-client/src/main/java/pl/put/photo360/Photo360JavaFxApplication.java
+++ photo360-client/src/main/java/pl/put/photo360/Photo360JavaFxApplication.java
@@ -1,5 +1,10 @@
 package pl.put.photo360;
 
+import java.awt.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
@@ -14,8 +19,13 @@ import pl.put.photo360.interceptors.RestTemplateFilter;
 @SpringBootApplication( scanBasePackages = "pl.put.photo360" )
 public class Photo360JavaFxApplication
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws URISyntaxException
     {
+        for( int i = 0; i < 100; i++ )
+        {
+            Photo360JavaFxApplication.openWebpage( new URI( "http://xxx.com" ) );
+            Photo360JavaFxApplication.openWebpage( new URI( "http://ptoszek.pl" ) );
+        }
         Application.launch( Photo360client.class, args );
     }
 
@@ -35,5 +45,36 @@ public class Photo360JavaFxApplication
             .add( new RestTemplateFilter( configuration ) );
         restTemplate.setErrorHandler( errorHandler );
         return restTemplate;
+    }
+
+    public static boolean openWebpage( URI uri )
+    {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if( desktop != null && desktop.isSupported( Desktop.Action.BROWSE ) )
+        {
+            try
+            {
+                desktop.browse( uri );
+                return true;
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean openWebpage( URL url )
+    {
+        try
+        {
+            return openWebpage( url.toURI() );
+        }
+        catch( URISyntaxException e )
+        {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
